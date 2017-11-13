97018128745ae3a9f5d49074aaaf8bb7ab663583
Joseph Hesketh <joe.m.hesketh@gmail.com>
2017-11-13 10:09:11 +0000

    Initial commit

---
+++ Potion Commotion/Assets/TitleMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMovement : MonoBehaviour {
+
+    public float magnitudeXMultiplier = 1.0f;
+    public float frequencyXMultiplier = 0.1f;
+    public float magnitudeYMultiplier = 1.0f;
+    public float frequencyYMultiplier = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.left * Mathf.Sin(Time.time * frequencyXMultiplier) * magnitudeXMultiplier
+            + Vector3.up * Mathf.Sin(Time.time * frequencyYMultiplier) * magnitudeYMultiplier;
+	}
+}
---
+++ Potion Commotion/Assets/TitleSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using InControl;
+
+public class TitleSceneManager : MonoBehaviour {
+
+    int selectedItem;
+
+    public Button[] menuOptions;
+
+    private void Start() {
+        menuOptions[0].Select();
+        selectedItem = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (InputManager.ActiveDevice.LeftStick.Down.WasPressed)
+        {
+            menuOptions[selectedItem].Se
+        }
+	}
+}
